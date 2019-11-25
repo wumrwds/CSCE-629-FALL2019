8c3d76b505dae387d1ba26cb932ab1b06b4948a1
Minreng Wu <wumrwds@gmail.com>
2019-11-25 01:08:09 -0600

    feat: add a method for getting edge from the graph by two vertices

--- src/main/java/edu/tamu/wumrwds/entity/Graph.java
+++ src/main/java/edu/tamu/wumrwds/entity/Graph.java
@@ -1,6 +1,8 @@
 package edu.tamu.wumrwds.entity;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Graph {
 
@@ -8,9 +10,16 @@ public class Graph {
 
     private List<Edge> edges;
 
+    private Map<String, Edge> edgeDict;
+
     public Graph(List<Vertex> vertices, List<Edge> edges) {
         this.vertices = vertices;
         this.edges = edges;
+
+        edgeDict = new HashMap<>();
+        for (Edge edge : edges) {
+            edgeDict.put(String.format("%s-%s", edge.getV1().getId(), edge.getV2().getId()), edge);
+        }
     }
 
 
@@ -29,6 +38,18 @@ public class Graph {
             degreeSum += vertex.degree();
         }
         return degreeSum;
+    }
+
+    public Vertex getVertxById(int vertexId) {
+        return vertices.get(vertexId);
+    }
+
+    public int getWeight(Vertex v1, Vertex v2) {
+        if (v1.getId() < v2.getId()) {
+            return edgeDict.get(String.format("%s-%s", v1.getId(), v2.getId())).getWeight();
+        }
+
+        return edgeDict.get(String.format("%s-%s", v2.getId(), v1.getId())).getWeight();
     }
 
     public double averageDegree() {
